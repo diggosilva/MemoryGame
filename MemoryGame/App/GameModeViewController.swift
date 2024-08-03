4aea1517de6a6bd6fe1d29c1abad17cb11e6e2d5
Diggo <rdiggosilva@gmail.com>
2024-08-03 11:04:51 -0300

    Rename ViewController to GameModeViewController

---
+++ MemoryGame/App/GameModeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameModeViewController.swift
+//  MemoryGame
+//
+//  Created by Diggo Silva on 02/08/24.
+//
+
+import UIKit
+
+class GameModeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
