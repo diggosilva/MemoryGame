6e4b35e234f54d577b24e1b3c601cdce2f29088f
Diggo <rdiggosilva@gmail.com>
2024-08-07 15:39:36 -0300

    Implemented the function that creates and shuffles the emojis on the buttons, and the function that when clicking on the card, the card flips and reveals the emoji

--- MemoryGame/App/Scenes/EasyBoardView.swift
+++ MemoryGame/App/Scenes/EasyBoardView.swift
@@ -82,41 +82,55 @@ class EasyBoardView: UIView {
     weak var delegate: BoardViewDelegate?
     
     private var action: (() -> Void)?
+   
+    private let cardBackColor = UIColor.systemRed
     
+    private var emojisShuffled = [String]() // Array de emojis embaralhados
+    
+    private lazy var buttons: [UIButton] = [button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11]
+
     init(action: (() -> Void)? = nil) {
         super.init(frame: .zero)
         self.action = action
         setupView()
+        emojis() // Inicializa os emojis
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configButton() -> UIButton {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .systemRed
-        button.layer.cornerRadius = 10
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOffset = CGSize(width: 5, height: 5)
-        button.layer.shadowOpacity = 0.3
-        button.layer.shadowRadius = 3.0
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        return button
+    func emojis() {
+        emojisShuffled = (easyModeVehicles + easyModeVehicles).shuffled()
+        for (index, button) in buttons.enumerated() {
+            button.setTitle("", for: .normal)
+        }
+        print("DEBUG: \(emojisShuffled)")
     }
     
-    private func configStack(axis: NSLayoutConstraint.Axis, arrangedSubviews views: [UIView]) -> UIStackView {
-        let stack = UIStackView(arrangedSubviews: views)
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = axis
-        stack.spacing = 20
-        stack.distribution = .fillEqually
-        return stack
+    @objc func buttonTapped(sender: UIButton) {
+        flipCard(sender)
     }
     
-    @objc func buttonTapped() {
-        action?()
+    private func flipCard(_ button: UIButton) {
+        let transitionOptions: UIView.AnimationOptions = [.transitionFlipFromRight, .curveEaseInOut]
+
+        UIView.transition(with: button, duration: 0.6, options: transitionOptions, animations: {
+            if button.title(for: .normal) == "" {
+                // Revela o emoji
+                if let index = self.buttons.firstIndex(of: button) {
+                    button.setTitle(self.emojisShuffled[index], for: .normal)
+                    button.layer.borderWidth = 2
+                    button.layer.borderColor = self.cardBackColor.cgColor
+                    button.backgroundColor = .white
+                    button.titleLabel?.font = .systemFont(ofSize: 40)
+                }
+            } else {
+                // Oculta o emoji mostrando o verso da carta
+                button.setTitle("", for: .normal)
+                button.backgroundColor = .systemRed
+            }
+        }, completion: nil)
     }
     
     private func setupView() {
@@ -137,5 +151,28 @@ class EasyBoardView: UIView {
             vStack.widthAnchor.constraint(lessThanOrEqualTo: vStack.widthAnchor, multiplier: 0.6),
             vStack.heightAnchor.constraint(equalToConstant: 340),
         ])
+    }
+    
+    private func configButton() -> UIButton {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = cardBackColor // Define a cor sólida como fundo
+        button.setTitle("", for: .normal) // Inicialmente define o título como vazio
+        button.layer.cornerRadius = 10
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 5, height: 5)
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowRadius = 3.0
+        button.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchUpInside)
+        return button
+    }
+    
+    private func configStack(axis: NSLayoutConstraint.Axis, arrangedSubviews views: [UIView]) -> UIStackView {
+        let stack = UIStackView(arrangedSubviews: views)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = axis
+        stack.spacing = 20
+        stack.distribution = .fillEqually
+        return stack
     }
 }
